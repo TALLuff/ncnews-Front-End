e1af24024699d8394d1e3ea06567efa8209ab45d
TALLuff <talluff@gmail.com>
2019-06-21 03:30:32 +0100

    Enlarge the click area and add animations on hover

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -6,23 +6,23 @@ function ArticlesList({ articles }) {
     <div>
       {articles.map(article => {
         return (
-          <div key={article.article_id} className="listItem">
-            <div>
-              <Link to={`/articles/${article.article_id}`}>
+          <Link to={`/articles/${article.article_id}`}>
+            <div key={article.article_id} className="listItem articleListItem">
+              <div>
                 <h3 className="listItemTitle">
                   <span className="red">></span>
                   {article.title.slice(0)}
                 </h3>
-              </Link>
-              Topic: {article.topic[0].toUpperCase() + article.topic.slice(1)},
-              Author: {article.author}, Created:{" "}
-              {article.created_at.slice(0, 10)}, Comments:{" "}
-              {article.comment_count}, Votes: {article.votes}
+                Topic: {article.topic[0].toUpperCase() + article.topic.slice(1)}
+                , Author: {article.author}, Created:{" "}
+                {article.created_at.slice(0, 10)}, Comments:{" "}
+                {article.comment_count}, Votes: {article.votes}
+              </div>
+              <div className="voteButtons">
+                <h2 className="votes">{article.votes}</h2>
+              </div>
             </div>
-            <div className="voteButtons">
-              <h2 className="votes">{article.votes}</h2>
-            </div>
-          </div>
+          </Link>
         );
       })}
     </div>
